bb22653e52a457d2ed1c4568f7eaaa21772a1e6a
Vítor Ian <goncalvesvitorian@gmail.com>
2024-06-19 18:50:36 -0300

    fix: new home add: ebook links fix: alterando email add: comentarios para melhorias futuras

--- js/index.js
+++ js/index.js
@@ -90,7 +90,7 @@ gsap.to('#contact',0,{display:"none"});
    gsap.to('#about',0,{display:"block",delay:.7});
    gsap.to('#navigation-content',0,{display:'flex',delay:2});
  })
- $('#contact-link').on('click',function(){
+ $('.contact-link').on('click',function(){
    gsap.to('#navigation-content',0,{display:"none",delay:.7});
    gsap.to('#navigation-content',0,{y:'-100%',delay:.7});
 gsap.to('#header',0,{display:"none"});
