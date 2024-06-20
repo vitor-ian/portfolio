889a59a6af5e68720bce48238c291389ff11012f
Vítor Ian <goncalvesvitorian@gmail.com>
2024-06-20 12:31:51 -0300

    fix: home page add: about me page

--- js/index.js
+++ js/index.js
@@ -104,7 +104,7 @@ gsap.to('#breaker-two',0,{display:"none",delay:2});
 gsap.to('#contact',0,{display:"block",delay:.7});
 gsap.to('#navigation-content',0,{display:'flex',delay:2});
 })
-$('#portfolio-link').on('click',function(){
+$('.portfolio-link').on('click',function(){
   gsap.to('#navigation-content',0,{display:"none",delay:.7});
   gsap.to('#navigation-content',0,{y:'-100%',delay:.7});
 gsap.to('#header',0,{display:"none"});
